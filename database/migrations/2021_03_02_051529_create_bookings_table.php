200a7dc4c2175939def04df382cf167ee540e08a
Nashir <nashiruddin.abubakar@gmail.com>
2021-03-02 12:52:17 +0700

    [ADD] add model and migration for booking table

---
+++ database/migrations/2021_03_02_051529_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('driver_id');
+            $table->integer('passenger_id');
+            $table->string('state');
+            $table->integer('country_id');
+            $table->double('fare', 8, 2);
+            $table->timestamp('created_at_local');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
